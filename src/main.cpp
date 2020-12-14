a89799c57aa3f58ec4dae83e714ea9ec0c0474f5
Emir Ensar <64320256+rahmanlar@users.noreply.github.com>
2020-12-14 12:04:59 +0300

    Fix cd function, add some comments

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,11 @@
 Copyright (c) 2020 Emir Ensar All Rights Reserved.
 Distributed under the terms of the MIT License.
 */
+// https://cezeri.software
 
 // Huge thanks for ferhatgec(https://github.com/ferhatgec)
 
-// include part from the ferhatgec/scrift
+// Include
 #include <ctype.h>
 #include <dirent.h>
 #include <unistd.h>
@@ -14,14 +15,13 @@ Distributed under the terms of the MIT License.
 #include <cstdio>
 #include <string.h>
 #include <stdio.h>
-#include <locale.h>
-#include <ctype.h>
 #include <thread>
 #include <string>
 #include <ctime>
+// ExecutePlusPlus from ferhatgec/execplusplus
 #include "../include/ExecutePlusPlus.hpp"
 
-
+// Program
 using namespace std;
 int main() {
   ExecutePlusPlus ex;
@@ -30,19 +30,20 @@ int main() {
   time_t ttime = time(0);
   char* dt = ctime(&ttime);
   gethostname(hostname, HOST_NAME_MAX);
-  cout << "\033[1;34m" << "Welcome to IntelliShell, " "\033[0m" <<  "\033[0;92m" << getenv("USER") << "\033[0m" << "!\n";
+  cout << "\033[1;34m" << "Welcome to IntelliShell, " "\033[0m" <<  "\033[0;92m" << getenv("USER") << "\033[0m" << "\033[1;34m" "!\n" << "\033[0m";
   cout << "\033[1;34m" << "The current local date and time is: " << "\033[0m" << "\033[0;92m" << dt;
   std::string shell;
     do {
       cout << "\033[1;34m" << getenv("USER") << "\033[0m"<< "\033[0;92m" << "@" << "\033[0m" << "\033[1;34m" << hostname << "\033[0m" << "\033[93m" << " | " << "\033[0m" << "\033[0;31m" << get_current_dir_name() << "\033[0m" << "\033[0;92m" " -> " << "\033[0m";
       getline(cin, shell);
-      ex.RunFunction(shell.c_str());
-    }
-   while(shell.find("cd"));
+  ex.RunFunction(shell.c_str());
+    while(shell.find("cd"));
     std::string choosedirectory;
      cout << "Choose a directory: ";
      getline(cin, choosedirectory);
      chdir(choosedirectory.c_str());
-    while(shell != "exit");
+    }
+   while(shell != "exit");
+   
   return 0;
 }
